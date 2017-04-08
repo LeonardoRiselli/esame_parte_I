892dddbaa2a49ad199c610ec750a947186efab57
LeonardoRiselli <juniorriselli@gmail.com>
2017-04-08 08:50:05 +0200

    TempoVolo Prova

--- TempoVolo.py
+++ TempoVolo.py
@@ -3,5 +3,8 @@ corario=float (input ("dimmi il consumo in GALLONI/H: " ))
 if carburante<0 or corario<0:
   print "i dati inseriti risultano falsati"
 else:
-  tvolo=carburante/corario
-  print "Il tempo di volo e' di ",tvolo," H"
+  ore=carburante/corario
+  minuti=ore/60
+  secondi=minuti/60
+
+  print "Il tempo di volo e' di ",ore," H ",minuti," min ",secondi" s"
